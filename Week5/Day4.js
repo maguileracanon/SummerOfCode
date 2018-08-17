545dbda380e2acdd796278168b1247c35bdcf103
Mara Asus <maguileracanon@bournemouth.ac.uk>
2018-08-18 00:07:18 +0100

    day4

---
+++ Week5/Day4.js
@@ -0,0 +1,37 @@
+/*
+var a = "Sara lala".length;
+var b = "Bituin Callanta".lenth;
+
+if (b>a){
+	console.log("They are equal in lenth.");
+} else if (a==20) {
+	console.log("length is twenty");
+}
+else if (a==21){
+	console.log("Length is twenty-one");
+} else {
+	console.log("a isnt bigger than b",a,b);
+}
+
+var input=''
+
+while (input != "bye"){
+	input = prompt()
+}
+
+alert("Come again soon!")
+
+*/
+var students2 = new Array("","Jessica Sanchez", "Nwamaka Okafor", "Sara Gottschalk",
+                "Mara Catalina", "Bituin Callanta","Anusha Lihala",
+                "Ana Sustic","Rox Arten", "Angela Balyseviene", "Cristina Tarantino")
+
+var students1 = students2[2]
+console.log(students1)
+
+
+// for loop 
+
+for (i=0 ;i<1000001; i++){
+	console.log(i)
+}
