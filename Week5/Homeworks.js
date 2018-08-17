9fe343222a6bcdb7a486bd283fcb9f11201dfe26
Mara in ORI <maguileracanon@bournemouth.ac.uk>
2018-08-17 18:01:32 +0100

    Homeworks w5 not complete

--- Week5/Homeworks.js
+++ Week5/Homeworks.js
@@ -43,4 +43,36 @@ rot_around_earth = 1/29.5
 console.log("The moon moves", 360*rot_around_earth , "degrees per day around the earth")
 
 
-/////////////////////////////////////////////
+/////////////////////// DAY 3  //////////////////////
+//Full name greeting. Write a program that asks for a person’s first name, then middle, and then last.
+// Finally, it should greet the person using their full name.
+first_name = prompt("Please type your first name: ")
+middle_name = prompt("Please type your middle name (press enter if you dont have one):")
+last_name = prompt("Please type your last name")
+alert("Wellcome "+ first_name +" " + middle_name+ " " + last_name)
+
+//Bigger, better favorite number. Write a program that asks for a person’s favorite number. Have your program add 1 to the number,
+//and then suggest the result as a bigger and better favorite number. (Do be tactful about it, though.)
+fav_number = prompt("Type your favourite number:")
+alert("Would you consider " + String(parseFloat(fav_number) + 1 )+  " as your bigger and better favorite number?")
+
+//Angry boss. Write an angry boss program that rudely asks what you want. 
+//Whatever you answer, the angry boss should yell it back to you and then fire you.
+
+wish = prompt("What do you want now? :")
+alert("WHADDAYA MEAN "+ wish.toUpperCase()+ "?!? YOU'RE FIRED!!")
+
+/*
+Table of contents. Here’s something for you to do in order to play around more with center, ljust, and rjust: write a program that will display a table of contents so that it looks like this:
+Table of Contents
+
+Chapter 1: Getting Started page 1 Chapter 2: Numbers page 9 Chapter 3: Letters page 13
+
+Optional: in JS we may prefer to 'print' these to the HTML file itself rather than the console.
+*/
+
+
+/////////////////////// DAY 4  //////////////////////
+
+
+
